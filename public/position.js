eda45686f8b236e2da94a904bdd91420040588da
DeepCFoxXx <learncode23@gmail.com>
2018-04-17 13:39:47 +0100

    move up added

---
+++ public/position.js
@@ -0,0 +1,13 @@
+var position = {
+  x:300,
+  y:100,
+  moveAmount:10,
+  setInBounds:function() {
+    if(this.x<0) this.x = 0;
+    if(this.y<0) this.y = 0;
+  },
+  moveUp:function() {
+    this.y = this.y - this.moveAmount;
+    this.setInBounds();
+  },
+};
