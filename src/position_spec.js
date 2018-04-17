d99fcb9a60e89e3351ed1027dd3a28238c759399
DeepCFoxXx <learncode23@gmail.com>
2018-04-17 14:33:42 +0100

    move down passing

--- src/position_spec.js
+++ src/position_spec.js
@@ -3,6 +3,7 @@ var position = require('./position.js');
 
 
 describe('position', function() {
+
   it('should start at position 0,0', function() {
     assert.equal(0, position.y);
     assert.equal(0, position.x);
@@ -10,6 +11,11 @@ describe('position', function() {
 
   it('should have a move amount of ten', function() {
     assert.equal(10, position.moveAmount);
+  });
+
+  it('should move down by move amount', function() {
+    position.moveDown();
+    assert.equal(10, position.y);
   });
 
 });
